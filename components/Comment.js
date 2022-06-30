58f4c1edc42dffba02121f412273483ed83eaf19
1luca1ether <lucacespedes@protonmail.com>
2022-06-30 13:12:36 -0300

    giscus

---
+++ components/Comment.js
@@ -0,0 +1,16 @@
+import { Giscus } from "@giscus/react";
+
+export default function Comment() {
+  return (
+    <Giscus
+      repo="melvnl/melvinliu.com"
+      repoId="R_kgDOHk-dUg"
+      category="General"
+      categoryId="DIC_kwDOHk-dUs4CP-Ao"
+      mapping="pathname"
+      reactionsEnabled="0"
+      emitMetadata="0"
+      theme="dark"
+    />
+  );
+}
